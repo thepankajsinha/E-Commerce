8d841f5555e8fa5dc27592e7de3e7d720dd0f97f
Pankaj Sinha <136917326+thepankajsinha@users.noreply.github.com>
2024-11-27 15:36:57 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
         <div className="flex flex-wrap justify-between items-center">
-          <Link to="/" className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex" > Monarque  </Link>
+          <Link to="/" className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex" > The Silk Thread  </Link>
 
           <nav className="flex flex-wrap items-center gap-4">
             <Link to={"/"} className="text-gray-300 hover:text-emerald-400 transition duration-300 ease-in-out" > Home </Link>
