3e159a62ebac7783949b2ebfdfc52cc9a016da14
thepankajsinha <askpankajsinha@gmail.com>
2024-12-24 21:54:32 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./lib/database.js";
-import path from "path";
+import cors from "cors";
+
 
 //import routes
 import authRoutes from "./routes/authRoute.js";
@@ -17,11 +18,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 
 //inbuilt middlewares
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
+app.use(cors());
+
 
 //use routes
 app.use("/api/auth", authRoutes);
@@ -29,14 +31,6 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/payments", paymentRoutes);
 
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 //listen server and connect database
 app.listen(PORT, () => {
